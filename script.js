1c9d92df99df16c1e232e8cf10fe63fac004bea2
David Jordânio Silveira <119808305+DaveSilveira@users.noreply.github.com>
2022-12-23 15:11:51 -0300

    reajustando

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-var elementos = getElementsByTagName(input);
-	elementos.style.backgroundColor = 'black';
+function gerar(){
+var recebe = document.getElementById('recebe');
 
+var maiuscula = document.getElementById('mai');
+var minuscula = document.getElementById('min');
+var numeros = document.getElementById('num');
+var simbolos = document.getElementById('simb');
+/*
+if(recebe == null){
+	
+}*/	
+}
