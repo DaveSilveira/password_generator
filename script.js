b47d3e7a6a85004019a5bcb423be2d1e8f31f763
David Jordânio Silveira <119808305+DaveSilveira@users.noreply.github.com>
2023-06-18 16:43:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-function gerar(){
-var recebe = document.getElementById('recebe');
 
-var maiuscula = document.getElementById('mai');
-var minuscula = document.getElementById('min');
-var numeros = document.getElementById('num');
-var simbolos = document.getElementById('simb');
-/*
-if(recebe == null){
-	
-}*/	
+const recebe = document.getElementById('recebe');
+const gerar = document.querySelector('#btn');
+const maiuscula = document.getElementById('mai');
+const minuscula = document.getElementById('min');
+const numeros = document.getElementById('num');
+const simbolos = document.getElementById('simb');
+
+const sim = ['!', '@', '#', '$', '%', ',', '.', '&', '*', '(', ')', '[', ']', '{', '}', '~', '^', '+', '=']
+const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const min = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'w', 'z']
+const mai = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'W', 'Z']
+
+gerar.addEventListener('click', function(){
+
+if(maiuscula.checked){
+    alert('teste')
 }
+
+});
