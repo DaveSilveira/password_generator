9bb047436367a9d96353f06971f4ed8a5586d28c
David Jordânio Silveira <davidjvsilveira@gmail.com>
2023-07-10 22:39:41 -0300

    ajustando as funções combinatorias

--- script.js
+++ script.js
@@ -12,37 +12,40 @@ const num = '0123456789'
 const min = 'abcdefghijklmnopqrstuvxywz'
 const mai = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ'
 
-function geraSenha(){
-    
+function detect2(x, y){
+        let checks = x + y
+       return geraSenha(checks)
+}
+
+
+function geraSenha(x){
+    let senha;
+    for(let i = 0; i < caracteres.value; i++){
+        let senhaAleatoria = Math.floor(Math.random() * x.length)
+        senha += x.charAt(senhaAleatoria)
+        recebe.textContent = senha 
+    }
+    return senha;
 }
 
 gerar.addEventListener('click', function(){
-    let senha;
-    if(maiuscula.checked){
-        for(let i = 0; i < caracteres.value; i++){
-            let senhaAleatoria = Math.floor(Math.random() * mai.length)
-            senha += mai.charAt(senhaAleatoria)
-            recebe.textContent = senha 
-        }
-    }if(minuscula.checked){
-        for(let i = 0; i < caracteres.value; i++){
-            let senhaAleatoria = Math.floor(Math.random() * min.length)
-            senha += min.charAt(senhaAleatoria)
-            recebe.textContent = senha 
-        }
-    }if(numeros.checked){
-        for(let i = 0; i < caracteres.value; i++){
-            let senhaAleatoria = Math.floor(Math.random() * num.length)
-            senha += num.charAt(senhaAleatoria)
-            recebe.textContent = senha 
-        }
-    }if(simbolos.checked){
-        for(let i = 0; i < caracteres.value; i++){
-            let senhaAleatoria = Math.floor(Math.random() * sim.length)
-            senha += sim.charAt(senhaAleatoria)
-            recebe.textContent = senha 
-        }
-    }
+    if(maiuscula.checked) geraSenha(mai)
+    if(minuscula.checked) geraSenha(min)
+    if(numeros.checked) geraSenha(num)
+    if(simbolos.checked) geraSenha(sim)
+    if(maiuscula.checked && minuscula.checked) detect2(mai, min)
+    if(maiuscula.checked && numeros.checked) detect2(mai, num)
+    if(maiuscula.checked && simbolos.checked) detect2(mai, sim)
+    if(minuscula.checked && numeros.checked) detect2(min, num)
+    if(minuscula.checked && simbolos.checked) detect2(min, sim)
+    if(numeros.checked && simbolos.checked) detect2(num, sim)
+    if(maiuscula.checked && minuscula.checked && simbolos.checked)
+    if(maiuscula.checked && minuscula.checked && numeros.checked)
+    if(maiuscula.checked && simbolos.checked && numeros.checked)
+    if(minuscula.checked && simbolos.checked && numeros.checked)
+// verificar ultima condicional
+
+    if(maiuscula.checked && minuscula.checked && simbolos.checked && numeros.checked);
 
 });
 
